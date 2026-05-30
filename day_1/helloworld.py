36f97bb75690be51da687ecb5c5cd8c22d2aaee1
cahxti21 <giancarlozulla@gmail.com>
2026-05-30 21:54:53 +0800

    trying out lists, and how Python reads them

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -16,3 +16,19 @@ print(type([1, 2, 3]))   # List - are ordered, mutable collections that allow du
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set - are unordered, mutable collections of completely unique elements
 print(type((9.8, 3.14, 2.7)))    # Tuple - are ordered, immutable collections that allow duplicates
+
+"""Mutable behavior - List, Set
+
+You can add, remove, or modify items directly inside a list
+
+e.g. - 
+"""
+
+my_list = [1, 2, 3] # define the list
+print(id(my_list))  # Example output: [1, 2, 3]
+
+# Modify an element in place
+my_list[0] = 99     # Python uses zero-based indexing, meaning the very first item is located at index 0
+print(id(my_list))  # Example output: [99, 2, 3]
+print(my_list)      # Output: [99, 2, 3]
+print(id(my_list))  # Output: 140237424 (The ID has NOT changed)
