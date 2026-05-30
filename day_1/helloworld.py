515372006198b170ee0b05c60cf2839ebf81ffdf
cahxti21 <giancarlozulla@gmail.com>
2026-05-30 19:43:28 +0800

    Initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,18 @@
+print(3 + 4)        # addition
+print(3 - 4)         # subtraction
+print(3 * 4)        # multiplication(*)
+print(3 / 4)        # division
+print(3 ** 4)       # exponential(**)
+print(3 % 4)        # modulus(%)
+print(3 // 4)       # floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(9.8))        # Float
+print(type(3.14))       # Float
+print(type(4 - 4j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
