9535424ad6230e31e45adba01a4df58b0c017d76
cahxti21 <giancarlozulla@gmail.com>
2026-06-02 19:24:55 +0800

    day 5 exercises update.

--- day_5/exercise1.py
+++ day_5/exercise1.py
@@ -1,1 +1,22 @@
 empty_list = []
+
+test_list = [1,2,3,4,5]
+
+print(len(test_list))
+
+print(test_list[0])
+print(test_list[2])
+print(test_list[-1])
+
+mixed_data_types = ["Gian", 23, 1.64, "Single", "Blk 979 Buangkok Crescent"]
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+
+print(it_companies)
+print(len(it_companies))
+print(it_companies[0])
+print(it_companies[3])
+print(it_companies[-1])
+
+it_companies[0] = "NVIDIA"
+print(it_companies)
+
