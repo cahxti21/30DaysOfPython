482baf313c7dca8ecc57439fdab7cb021a179c87
cahxti21 <giancarlozulla@gmail.com>
2026-06-03 21:14:22 +0800

    Add tuple.py with examples of tuple creation and usage

---
+++ day_6/tuple.py
@@ -0,0 +1,16 @@
+# Tuple - created using "()"
+
+empty = ()
+
+# or
+empty = tuple()
+
+# Tuple with items
+fruits = ("apple", "banana", "cherry")
+
+# Tuple with a single item - don't miss the comma
+fruits = ("apple",)
+
+# Tuple with multiple items
+fruits = ("apple", "banana", "cherry")
+
